f44fd0058f7b02120cf21cf9022b743b2a8adcd8
Mateo Siechert <86209620+mateogoat@users.noreply.github.com>
2026-04-15 22:03:05 -0700

    Layout Adjustments

--- JavaScript/data/storyData.js
+++ JavaScript/data/storyData.js
@@ -2,15 +2,15 @@ export const data = [
     {
         id: "Intro",
         scenes: [
-            { image: 'images/Visuals/Intro/intro1.png', text: "John wakes up smth smth" }, 
-            { image: 'images/Visuals/Intro/intro1.png', text: "It's so beautiful" }, 
-            { image: 'images/Visuals/Intro/intro1.png', text: "Errgh and then something else happens here" }, 
+            { image: 'images/Visuals/Intro/intro1.png', text: "John brushes his teeth, gets dressed, and begins his commute to work. He stretches outside, enjoying the vivid warmth of the sun." },
+            { image: 'images/Visuals/Intro/intro1.png', text: "It's so beautiful" },
+            { image: 'images/Visuals/Intro/intro1.png', text: "Errgh and then something else happens here" },
             {
                 image: 'images/Visuals/Intro/intro1.png',
                 text: "Woah! Make a choice, Johnny!",
                 options: [
-                    { text: "Go to work", leadsTo: "1A" /* Will probably not be what we actually call them */ }, 
-                    { text: "Ditch your job and wander off", leadsTo: "1B" }, 
+                    { text: "Go to work", leadsTo: "1A" /* Will probably not be what we actually call them */ },
+                    { text: "Ditch your job and wander off", leadsTo: "1B" },
                     { text: "Get a liiittle bit more sleep...", leadsTo: "1C" },
                 ]
             },
